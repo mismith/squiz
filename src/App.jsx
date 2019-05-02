b941736b3e09b7d609b9f556c4ef72dd15a397e6
Murray Smith <murray@mismith.info>
2019-05-01 18:03:38 -0600

    add basic 404 route/component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
 import Lobby from './views/Lobby';
 import Screen from './views/Screen';
 import Controller from './views/Controller';
+
+const NotFound = (props) => (
+  <Typography variant="h3" color="secondary" style={{margin: 'auto'}}>
+    Not found
+  </Typography>
+);
 
 export default () => (
   <BrowserRouter>
@@ -17,15 +24,17 @@ export default () => (
       />
       <Route
         path="/games/:gameID/players/:playerID"
+        exact
         render={props => <Controller {...props} {...props.match.params} />}
       />
       <Route
         path="/games/:gameID/:categoryID?/:playlistID?"
-        render={props => <Screen {...props} {...props.match.params} playerID="screen" />}
+        exact
+        render={props => <Screen {...props} {...props.match.params} />}
       />
       <Route
-        path="/"
-        render={props => <Lobby {...props} />}
+        path="/*"
+        component={NotFound}
       />
     </Switch>
   </BrowserRouter>
