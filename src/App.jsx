dfc1888c36f41a9b1d5302d08918a7056910ca4f
Murray Smith <murray@mismith.info>
2019-05-03 20:17:40 -0600

    prevent screen from sleeping (e.g. for Controller on phones)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
+import NoSleep from 'nosleep.js';
+
 import Lobby from './views/Lobby';
 import Screen from './views/Screen';
 import Controller from './views/Controller';
+
+const noSleep = new NoSleep();
 
 const NotFound = (props) => (
   <Typography variant="h3" color="secondary" style={{margin: 'auto'}}>
@@ -12,35 +16,56 @@ const NotFound = (props) => (
   </Typography>
 );
 
-export default () => (
-  <BrowserRouter>
-    <CssBaseline />
-    <Switch>
-      <Route
-        exact
-        strict
-        path="/:url+/"
-        render={props => <Redirect to={props.location.pathname.replace(/\/$/, '')}/>}
-      />
-      <Route
-        path="/"
-        exact
-        component={Lobby}
-      />
-      <Route
-        path="/games/:gameID/players/:playerID"
-        exact
-        render={props => <Controller {...props} {...props.match.params} />}
-      />
-      <Route
-        path="/games/:gameID/:categoryID?/:playlistID?"
-        exact
-        render={props => <Screen {...props} {...props.match.params} />}
-      />
-      <Route
-        path="/*"
-        component={NotFound}
-      />
-    </Switch>
-  </BrowserRouter>
-);
+export default () => {
+  // prevent screen from sleeping
+  useEffect(() => {
+    const add = () => {
+      noSleep.enable();
+      remove();
+    };
+    const remove = () => {
+      document.removeEventListener('click', add);
+      document.removeEventListener('touchstart', add);
+    };
+    document.addEventListener('click', add);
+    document.addEventListener('touchstart', add);
+
+    return () => {
+      noSleep.disable();
+      remove();
+    };
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <CssBaseline />
+      <Switch>
+        <Route
+          exact
+          strict
+          path="/:url+/"
+          render={props => <Redirect to={props.location.pathname.replace(/\/$/, '')}/>}
+        />
+        <Route
+          path="/"
+          exact
+          component={Lobby}
+        />
+        <Route
+          path="/games/:gameID/players/:playerID"
+          exact
+          render={props => <Controller {...props} {...props.match.params} />}
+        />
+        <Route
+          path="/games/:gameID/:categoryID?/:playlistID?"
+          exact
+          render={props => <Screen {...props} {...props.match.params} />}
+        />
+        <Route
+          path="/*"
+          component={NotFound}
+        />
+      </Switch>
+    </BrowserRouter>
+  );
+}
