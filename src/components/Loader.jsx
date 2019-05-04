675c68838fd4743087be6548d492b95a312fa90b
Murray Smith <murray@mismith.info>
2019-05-04 15:55:20 -0600

    allow styling Loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-export default (props) => (
-  <CircularProgress style={{margin: 'auto'}} {...props} />
+export default ({ style, ...props }) => (
+  <CircularProgress style={{margin: 'auto', ...style}} {...props} />
 );
