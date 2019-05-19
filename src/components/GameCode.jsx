d28f36b4628ce80ce3beccf251cc0b2eac459954
Murray Smith <murray@mismith.info>
2019-05-19 13:41:00 -0600

    use overline styling for GameCode label

--- src/components/GameCode.jsx
+++ src/components/GameCode.jsx
@@ -4,15 +4,13 @@ import Typography from '@material-ui/core/Typography';
 const styles = {
   label: {
     fontSize: 10,
-    textTransform: 'uppercase',
-    letterSpacing: '0.1em',
-    fontWeight: 'bold',
+    lineHeight: 1.5,
   },
 };
 
 export default ({ gameID }) => (
   <div style={{textAlign: 'center', margin: 'auto'}}>
     <Typography variant="h4">{gameID}</Typography>
-    <Typography color="textSecondary" style={styles.label}>Game Code</Typography>
+    <Typography variant="overline" color="textSecondary" style={styles.label}>Game Code</Typography>
   </div>
 );
