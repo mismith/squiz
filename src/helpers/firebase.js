1264a0304187f51129c5945e641de46149868447
Murray Smith <murray@mismith.info>
2020-10-28 19:38:16 -0600

    switch to dedicated firebase project

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -2,16 +2,18 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 export const config = {
-  apiKey: 'AIzaSyCwAPZxgorwuqX_7gku1XD1UY3YbUyPbq4',
-  authDomain: 'mismith.firebaseapp.com',
-  databaseURL: 'https://mismith.firebaseio.com',
-  projectId: 'firebase-mismith',
-  storageBucket: 'firebase-mismith.appspot.com',
-  messagingSenderId: '668345313266'
+  apiKey: 'AIzaSyBsV2vcnGc5HoQm8dZuLS-svp8N5Z6jGkg',
+  authDomain: 'squiz-9a2c0.firebaseapp.com',
+  databaseURL: 'https://squiz-9a2c0.firebaseio.com',
+  projectId: 'squiz-9a2c0',
+  storageBucket: 'squiz-9a2c0.appspot.com',
+  messagingSenderId: '521620949890',
+  appId: '1:521620949890:web:1005adcb3041c685b65d9b',
+  measurementId: 'G-RLXPT47RR7',
 };
 export const app = firebase.initializeApp(config);
 
 export const { FieldValue } = firebase.firestore;
-export const firestore = app.firestore().collection('apps').doc('squiz');
+export const firestore = app.firestore();
 
 export default firebase;
