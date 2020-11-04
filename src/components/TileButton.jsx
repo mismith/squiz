992f9ad6346cb3387e9768b52900911cfb7b27df
Murray Smith <murray@mismith.info>
2020-11-03 23:28:17 -0700

    grow TileButton once image loaded (if specified)

--- src/components/TileButton.jsx
+++ src/components/TileButton.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import ButtonBase from '@material-ui/core/ButtonBase';
+import { useAsync } from 'react-async-hook';
 
 const useStyles = makeStyles({
   button: {
     display: 'inline-flex',
     flexDirection: 'column',
     margin: 8,
+    transform: 'scale(1)',
     transition: 'transform 300ms',
 
     '&:hover': {
       transform: 'scale(1.05)',
+    },
+    '&:not(.imageLoaded)': {
+      transform: `scale(0)`,
     },
   },
   label: {
@@ -31,12 +36,35 @@ const useStyles = makeStyles({
   },
 });
 
-export default React.forwardRef(({ label, image, size = 200, className, style, children, ...props }, ref) => {
+const imgCache = {};
+async function loadImage(src, force = false) {
+  if (!src) return true;
+  if (!imgCache[src] || force) {
+    const img = document.createElement('img');
+    imgCache[src] = new Promise(resolve => {
+      img.onload = () => resolve(true);
+      img.onerror = () => resolve(false);
+      img.src = src;
+    });
+  }
+  return imgCache[src];
+}
+
+export default React.forwardRef(({
+  label,
+  image,
+  size = 200,
+  className,
+  style,
+  children,
+  ...props
+}, ref) => {
   const classes = useStyles();
+  const { result: isImageLoaded } = useAsync(loadImage, [image, true]);
 
   return (
     <ButtonBase
-      className={`${className} ${classes.button}`}
+      className={`${className} ${classes.button} ${isImageLoaded ? 'imageLoaded' : ''}`}
       style={{ ...style, width: size, height: size }}
       {...props}
       ref={ref}
